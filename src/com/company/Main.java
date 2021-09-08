84586adb17cf81cd6135aedae100294ab4ee5f7e
Moksleivis <Moksleivis@KITM.local>
2021-09-08 09:00:37 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, rez;
+        Scanner S = new Scanner(System.in);
+        System.out.println("Įveskite skaičių");
+        a = S.nextInt();
+        rez = 1 * a;
+        System.out.println("1*" + a + "=" + rez);
+        rez = 2 * a;
+        System.out.println("2*" + a + "=" + rez);
+        rez = 3 * a;
+        System.out.println("3*" + a + "=" + rez);
+        rez = 4 * a;
+        System.out.println("4*" + a + "=" + rez);
+        rez = 5 * a;
+        System.out.println("5*" + a + "=" + rez);
+        rez = 6 * a;
+        System.out.println("6*" + a + "=" + rez);
+        rez = 7 * a;
+        System.out.println("7*" + a + "=" + rez);
+        rez = 8 * a;
+        System.out.println("8*" + a + "=" + rez);
+        rez = 9 * a;
+        System.out.println("9*" + a + "=" + rez);
+        rez = 10 * a;
+        System.out.println("10*" + a + "=" + rez);
+    }
+}
